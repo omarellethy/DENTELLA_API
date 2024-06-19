5b49483a2865935ae05f04cb99a40bfdb6af96de
omarellethy <95581289+omarellethy@users.noreply.github.com>
2024-06-19 23:49:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ classes = [
     "Your Teeth are Good & You Don't Need To Visit Doctor"
 ]
 
-model = keras.models.load_model("model.pkl")
+model = keras.models.load_model("APP-host/code/models/imageclassifier.h5")
 
 def predict(img):
     img = img.resize((224, 224))
